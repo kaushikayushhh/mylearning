b0be38d61daded0b1c5fde7969fb81b5822d0423
Ayush kaushik <kaushik.ak8171@gmail.com>
2026-02-23 10:21:58 +0530

    Initial

---
+++ assignment1.py
@@ -0,0 +1,7 @@
+a=int(input('Enter first number: '))
+b=int(input('Enter second number: '))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("Addition: ",addition,"\nSubtraction: ",subtraction,"\nMultiplication: ",multiplication,"\nDivision: ",division)
---
+++ task2.py
@@ -0,0 +1,3 @@
+str1=str(input("Enter your first name:"))
+str2=str(input("Enter your last name:"))
+print(f"Hello,{str1+" "+str2}!Welcome to the python program ")
